aa2347523d1926a0257ff7e6cc91fdeffed163d1
daHaimi <daHaimi@users.noreply.github.com>
2020-05-20 10:20:30 +0200

    Create Buttons.ino

---
+++ Buttons.ino
@@ -0,0 +1,19 @@
+#define BUTTON1 16
+#define BUTTON2 14
+
+void initButtons() {
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
+  
+  attachInterrupt(digitalPinToInterrupt(BUTTON1), handleButton1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(BUTTON2), handleButton2, FALLING);
+}
+
+void handleButton1() {
+  Serial.printf("Pressed Button 1");
+}
+
+void handleButton2() {
+  Serial.printf("Pressed Button 2");
+  
+}
